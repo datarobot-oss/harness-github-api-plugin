291c33c7388295dc4d00a7946588aea0893db7be
Andrey Hura <andriy.hura@datarobot.com>
2024-12-06 15:07:08 +0200

    Update plugin to return pr id and not fail if pr exists (#3)

--- pullRequest.go
+++ pullRequest.go
@@ -2,15 +2,35 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"github.com/google/go-github/v62/github"
+	"log"
 	"strconv"
+	"strings"
 )
 
 func createPullRequest(client *github.Client, ctx *context.Context, repositoryName string, repositoryOwner string,
-	sourceBranch string, targetBranch string, title string, body string) {
-	_, _, err := client.PullRequests.Create(*ctx, repositoryOwner, repositoryName,
+	sourceBranch string, targetBranch string, title string, body string) map[string]string {
+	pr, _, err := client.PullRequests.Create(*ctx, repositoryOwner, repositoryName,
 		&github.NewPullRequest{Title: &title, Body: &body, Head: &sourceBranch, Base: &targetBranch})
-	failOnErr(err)
+	if err != nil {
+		ghErr, _ := err.(*github.ErrorResponse)
+		fmt.Println(err)
+		if strings.HasPrefix(ghErr.Errors[0].Message, "A pull request already exists") {
+			fmt.Println("Pull Request with requested head and base already exists. Getting it's Number.")
+			options := &github.PullRequestListOptions{
+				Head: repositoryOwner + ":" + sourceBranch,
+				Base: targetBranch,
+			}
+			prs, _, err := client.PullRequests.List(*ctx, repositoryOwner, repositoryName, options)
+			failOnErr(err)
+			pr = prs[0]
+		} else {
+			log.Fatal(err)
+		}
+	}
+	fields := map[string]string{"PR_NUMBER": strconv.Itoa(*pr.Number)}
+	return fields
 }
 
 func mergePullRequest(client *github.Client, ctx *context.Context, repositoryName string, repositoryOwner string, pullRequestNumber string, commitMessage string) {
