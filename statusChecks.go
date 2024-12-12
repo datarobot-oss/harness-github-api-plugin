bcdcdd065c9556a0f259c761f36cea594f0884d9
andrey-hura <andriy.hura@datarobot.com>
2024-12-12 21:58:05 +0200

    Add waitForStatus command

--- statusChecks.go
+++ statusChecks.go
@@ -3,8 +3,13 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"github.com/google/go-github/v62/github"
+	"log"
+	"slices"
+	"strconv"
 	"strings"
+	"time"
 )
 
 func setStatusCheck(client *github.Client, ctx *context.Context, repositoryName string, repositoryOwner string,
@@ -17,6 +22,41 @@ func setStatusCheck(client *github.Client, ctx *context.Context, repositoryName 
 	})
 
 	failOnErr(err)
+}
+
+func waitForStatus(client *github.Client, ctx *context.Context, repositoryName string, repositoryOwner string,
+	sha string, statusName string, timeoutMinutes string) map[string]string {
+	var result string
+	timeoutMinutesInt, errConv := strconv.Atoi(timeoutMinutes)
+	failOnErr(errConv)
+	// 1 attempt is 30 seconds
+	maxAttempts := timeoutMinutesInt * 2
+	for attempt := 1; attempt <= maxAttempts; attempt++ {
+		statuses, _, err := client.Repositories.ListStatuses(*ctx, repositoryOwner, repositoryName, sha, nil)
+		failOnErr(err)
+		slices.Reverse(statuses)
+		for _, status := range statuses {
+			if *status.Context == statusName {
+				result = *status.State
+			}
+		}
+		if result == "" {
+			fmt.Println("No status check with name " + statusName)
+		} else {
+			fmt.Println(statusName + " " + result)
+		}
+		if result == "success" || result == "failure" {
+			break
+		}
+		time.Sleep(30 * time.Second)
+	}
+
+	if result == "pending" {
+		log.Fatal("max attempts reached but result is still pending")
+	} else if result == "" {
+		log.Fatal("max attempts reached but status check never appears")
+	}
+	return map[string]string{"RESULT": result}
 }
 
 func listStatusChecks(client *github.Client, ctx *context.Context, repositoryName string, repositoryOwner string, sha string) map[string]string {
